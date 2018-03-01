bcb32735278d2151dec04701ad9996c001185cc5
OkBand <o.bandura42@gmail.com>
2018-03-01 14:23:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function getZerosCount(number) {
 var zeros = 0;
 for (i = 5; (number/i) >= 1; i = i * 5) {
-  zeros = zeros + Math.floor(number/i);
+  zeros = zeros + Math.floor(number/i); //The number of zeros in a factorial = the number of 5's in the multiplication
 };
 return zeros;
 }
